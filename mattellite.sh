fb3d939d963d3d8defccd8f7815ebf4e55a00d76
endgrid <104172903+endgrid@users.noreply.github.com>
2022-10-03 23:19:06 -0500

    Update mattellite.sh

--- mattellite.sh
+++ mattellite.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 date=`date +%F`
 cd /root/scans
-nmap -v -T4 -sn 192.168.0.0/24 -oX scan-$date.xml > /dev/null
+nmap -T4 -v 192.168.0.0/24 -oX scan-$date.xml > /dev/null
 if [ -e scan-prev.xml ] ; then
         ndiff -v scan-prev.xml scan-$date.xml > diff-$date
         echo -e "Network changes since last scan: \n$(cat diff-$date)" | mail -s "$(date +%D) Network report" recipient@gmail.com
